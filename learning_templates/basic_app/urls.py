6da772d2d792d7732617cd78ef2b50a54d3235d5
lifengyan-amber <lifengyan-amber@users.noreply.github.com>
2020-10-18 18:10:24 -0400

    initialize project

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from basic_app import views
+from django.conf.urls import url
+
+#TEMPLATE TAGGING
+app_name = 'basic_app'
+
+urlpatterns = [
+    url(r'^flowchart/$', views.flowchart, name = 'flowchart'),
+    url(r'^quiz/$', views.quiz, name = 'quiz'),
+]
---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+
+def quiz(request):
+    return render(request, 'basic_app/quiz.html')
+
+
+def flowchart(request):
+    return render(request, 'basic_app/flowchart.html')
+
+
