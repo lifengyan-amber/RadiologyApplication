5b5802c81d02cfd1b94f91a2312fa2ae1d4e12f4
lifengyan-amber <lifengyan-amber@users.noreply.github.com>
2020-10-21 22:37:50 -0400

    update quiz

--- learning_templates/static/quizdata/quizdata.js
+++ learning_templates/static/quizdata/quizdata.js
@@ -13,18 +13,18 @@ var quiz = {
         1: {
             "question"      :   "What kind of measurements do you use",
             "choices"       :   {
-                                    "Single": 5,
-                                    "Multiple": 6
+                                    "Single measurements": 5,
+                                    "Mean of multiple measurements": 6
                                 },
             "selection"       :   -1,
             "previous"      :   0,
             "results": -1
         },
         2: {
-            "question"      :   "What kind of model do you use",
+            "question"      :   "Does the model satisfy ANOVA assumption",
             "choices"       :   {
-                                    "ANOVA": 3,
-                                    "Not Anova": 0
+                                    "Yes": 3,
+                                    "Not Anova": 9
                                 },
             "selection"       :   -1,
             "previous"      :   0,
@@ -33,19 +33,44 @@ var quiz = {
         3: {
             "question"      :   "How do raters read",
             "choices"       :   {
-                                    "Same set": 5,
-                                    "Multiple": 6
+                                    "Same set of raters for all subjects": 7,
+                                    "Each subject rated by different set of raters": -1
                                 },
             "selection"       :   -1,
             "previous"      :   2,
             "results": -1
         },
         5 : {
-            "results": "ICC(k,A)",
+            "results": "ICC(1,A)",
             "previous":   1
         },
         6 : {
-            "results": "ICC(1,A)",
+            "results": "ICC(k,A)",
             "previous":   1
-        }
+        },
+        7: {
+            "question"      :   "How are raters selected?",
+            "choices"       :   {
+                                    "Specific set of raters": 8,
+                                    "Random set of raters": -1
+                                },
+            "selection"       :   -1,
+            "previous"      :   3,
+            "results": -1
+        },
+        8: {
+            "question"      :   "How many raters",
+            "choices"       :   {
+                                    "Single": -1,
+                                    "Multiple": -1
+                                },
+            "selection"       :   -1,
+            "previous"      :   7,
+            "results": -1
+        },
+        9 : {
+            "results": "CCC",
+            "previous":   2
+        },
+        
     };
